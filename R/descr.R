b19bc4ca8a625df8bc4f330a6065152339690ef9
juanchiem <edwardsmolina@gmail.com>
2022-03-14 20:32:21 -0300

    descr function

---
+++ R/descr.R
@@ -0,0 +1,18 @@
+#' Compute descriptive statistics for the numeric columns of a data frame.
+#' @param x The numerical variable to describe
+#' @param ... Optional. Columns in the data frame
+#' @return A data frame with descriptive statistics.
+#' @examples
+#' \dontrun{
+#' descr(iris$Sepal.Length)
+#' }
+
+descr <- function(x){
+  range <- paste( c("range:"), round(range(x)[1],1), "to", round(range(x)[2],1))
+  mean <- paste( c("mean"), round(mean(x),1), sep = ": ")
+  median <- paste( c("median"), round(median(x),1), sep = ": ")
+  IQR <- paste( c("IQR"), round(IQR(x),1), sep = ": ")
+  box <- paste( c('box'), round(quantile(x, 0.25),1), "-", round(quantile(x, 0.75),1) )
+  result <- list(range, mean, median, IQR, box)
+  return(result)
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# cptr
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of cptr is to organize your procedures for reporting results from crop protection trials. 
+
+# Installation
+
+You can install the released version of cptr from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("cptr")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(cptr)
+## basic example code
+```
+
